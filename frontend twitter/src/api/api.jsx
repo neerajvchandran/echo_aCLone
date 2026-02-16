2165c88ba5e30d07d27fdc261969405009f6b944
Neeraj V Chandran <neerajvchandran1931@gmail.com>
2026-02-16 13:55:32 +0530

    Frontend edit

--- frontend twitter/src/api/api.jsx
+++ frontend twitter/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://echo-backend-2-mc24.onrender.com/api",
   withCredentials: true,
 });
 
